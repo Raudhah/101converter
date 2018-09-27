44f3bc69e6b925c287d67e724bcf9460dc38fd51
toni tegar sahidi <tonitegarsahidi@gmail.com>
2018-09-27 09:17:24 +0700

    pindah pakai VS code

--- content/unit/temperature/converter-fahrenheit-to-celsius.php
+++ content/unit/temperature/converter-fahrenheit-to-celsius.php
@@ -25,34 +25,95 @@
                   <li class="active">Fahrenheit to Celsius</li>
                 </ol>
 
+                <!-- THIS IS THE PROGRAM PART -->
 
-                  <h1 id="sec0">Converter Fahrenheit to Celsius</h1>
+                  <h2 id="sec0">Converter Fahrenheit to Celsius</h2>
                   Convert Fahrenheit to Celcius Online here. See also how it works, how to do it manually, and More! Even the source code is available here.
 
+                  <hr/>
 
 
-                	<hr class="col-md-12">
+                  <div class="row">
+                    <div class="col-md-12">
+                        <strong><em>
+                        Please input The temperature in Fahrenheit below. Click Convert to see the result. You can copy the result to clipboard.
+                        </em></strong>
+                      <br/>
+                      <br/>
+
+                        <form class="form-horizontal">
+
+                          <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">Temperature in Fahrenheit</label>
+                            <div class="col-sm-10">
+                              <input type="number" class="form-control input-lg" id="inputTempF" placeholder="Enter Temperature">
+                            </div>
+                          </div>
+
+                          <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <button  id="convertbutton"  type="button" class="btn btn-success btn-lg">Convert</button>
+                            </div>
+                          </div>
+
+                          <div class="form-group">
+                           <label class="col-sm-2 control-label">Result</label>
+                           <div class="col-sm-10">
+
+                              <div class="input-group">
+                               <input type="text" readonly class="form-control input-lg" id="convertResult" placeholder="Result">
+                               <span class="input-group-addon" id="basic-addon2">Fahrenheit</span>
+                              </div>
 
 
-                	<h2 id="sec1">Content</h2>
-                	<p>
-                  Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                  dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.
-                	</p>
+                             <button id="copybutton" data-toggle="tooltip" title="Copy the Result to Clipboard" data-clipboard-target="#result" type="button" class="btn btn-primary btn-xs">Copy The Result</button>
+
+                             </p>
+                           </div>
+                         </div>
+                        </form>
+
+
+
+                    </div>
+                  </div>
+
+                  <hr class="col-md-12">
+
+
+                  <!-- THIS IS THE DESCRIPTION ABOUT HOW IT WORKS PART -->
+
                 	<div class="row">
-                    <div class="col-md-6">
-                      <div class="panel panel-default">
-                        <div class="panel-heading"><h3>Hello.</h3></div>
-                        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate.
-                        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
-                        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan.
-                        Aliquam in felis sit amet augue.
+                    <div class="col-md-10">
+                      <div class="panel panel-primary">
+                        <div class="panel-heading"><h4><strong>Understand the Basic and How To Do it Manually</strong></h4></div>
+                        <div class="panel-body">
+                          <p>in this part we explain on how it all works. </p>
+
+
                         </div>
                       </div>
                     </div>
-                    <div class="col-md-6">
+
+                    <!-- THIS IS THE FORMULA PART -->
+
+                    <div class="row">
+                      <div class="col-md-10">
+                        <div class="panel panel-primary">
+                          <div class="panel-heading"><h4><strong>THE FORMULA</strong></h4></div>
+                          <div class="panel-body">
+                            <p>Temp Celsius = (Temp Fahranheit * 9 / 5) + 32</p>
+
+                          </div>
+                        </div>
+                      </div>
+
+
+                    <!-- THIS IS THE SOURCE CODE -->
+
+                    <div class="col-md-10">
                         <div class="panel panel-default">
-                        <div class="panel-heading"><h3>Hello.</h3></div>
+                        <div class="panel-heading"><h4>Source Code.</h4></div>
                         <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate.
                         Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
                         dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan.
@@ -63,68 +124,6 @@
                 	</div>
 
                 	<hr>
-
-                	<h2 id="sec2">Section 2</h2>
-                	<p>
-                  Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                  dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores
-                  eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
-                	</p>
-                	<div class="row">
-                		<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                    	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                    	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                	</div>
-
-                  <h3 id="sec2a">Section 2 a</h3>
-                	<p>
-                  Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                  dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores.
-                  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores
-                  eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
-                    <br><br>
-                	</p>
-
-                  <h3 id="sec2b">Section 2 b</h3>
-                	<p>
-                  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores. Nemo enim ipsam voluptatem quia voluptas
-                  sit aspernatur aut odit aut fugit, sed quia cor magni dolores. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores
-                  eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. <br><br>
-                	</p>
-
-
-                	<hr>
-
-                	<h2 id="sec3">Section 3</h2>
-        			Images are responsive sed @mdo but sum are more fun peratis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                  totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                  dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores
-                  eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!!<br><br>
-
-
-                  <hr>
-
-                	<h2 id="sec4">Section 4</h2>
-        			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                  totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                  dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores
-                  eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                  sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut<br><br>
-
 
 
 
@@ -133,3 +132,56 @@
 
       include "../../../footer.php";
        ?>
+
+
+
+
+       <script>
+       // THE MAIN PROGRAM CODE IS HERE
+
+
+            //first built the function
+           function convertFahrenheitToCelsius(tempF){
+
+             var tempC;
+             tempC = (9.0 * tempF / 5.0) + 32;
+             return tempC;
+           }
+
+           //now put the function to the places
+           $( "#convertbutton" ).click(function() {
+
+             var inputTempF = document.getElementById("inputTempF");
+             var convertResult = document.getElementById("convertResult");
+
+             var result = convertFahrenheitToCelsius(inputTempF.value);
+
+             convertResult.value = result;
+
+             console.log("Result : " + result + " Fahrenheit");
+
+           });
+
+           //copy to clipboard button
+           function copyResult(){
+
+             /* Get the text field */
+              var copyText = document.getElementById("convertResult");
+
+              /* Select the text field */
+              copyText.select();
+
+              /* Copy the text inside the text field */
+              document.execCommand("copy");
+
+              /* Alert the copied text */
+              alert("Success Copied the Result : " + copyText.value);
+           }
+
+           $( "#copybutton" ).click(function() {
+             copyResult();
+           });
+
+
+
+       </script>
